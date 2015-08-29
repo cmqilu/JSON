211a7820e6b2d7365e3439055d7affa1bba2f6a0
wf <cmqilu@stumail.nwu.edu.cn>
2015-08-29 16:24:34 +0800

    the first day of JSON study
    JSON(轻量级数据交换格式)和serialize(PHP)比较

---
+++ JSON.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+	function createHtmlTag($tag = ""){
+		echo"<h1>$tag</h1><br />";
+	}
+	
+	createHtmlTag("hello");
+	
+	createHtmlTag("JSON和serialize 对比");
+	
+	$member = array("username","age");
+	
+	var_dump($member);
+	
+	$jsonObj = json_encode($member);
+	
+	$serializeObj = serialize($member);
+	
+	createHtmlTag($jsonObj);
+	
+	createHtmlTag($serializeObj);
